4eef8d8b20b497419e7659086a232d16010c525f
pgarrett <pgarrett@scripps.edu>
2024-06-18 17:50:11 -0700

    Add theme customization component for Streamlit
    This commit introduces a new Streamlit component, `stthemes`, allowing users to customize app themes. Various functions have been added to control theme attributes like background color, primary color, text color

---
+++ src/stthemes/constants.py
@@ -0,0 +1,78 @@
+from typing import Dict, List
+
+VALID_FONTS: List[str] = ["sans serif", "serif", "monospace"]
+
+THEMES: Dict[str, str] = {
+    'Beach': {
+        'backgroundColor': '#f1f0e8',
+        'primaryColor': '#3282B8',
+        'secondaryBackgroundColor': '#eee0c9',
+        'textColor': '#45474b'
+    },
+    'Pineapple': {
+        'backgroundColor': '#f4ce14',
+        'primaryColor': '#495e57',
+        'secondaryBackgroundColor': '#f5f7f8',
+        'textColor': '#45474b'
+    },
+    'Peachy': {
+        'backgroundColor': '#ffeecc',
+        'primaryColor': '#F38181',
+        'secondaryBackgroundColor': '#ffddcc',
+        'textColor': '#B83B5E'
+    },
+    'Tropical': {
+        'backgroundColor': '#faf1e4',
+        'primaryColor': '#9eb384',
+        'secondaryBackgroundColor': '#cedebd',
+        'textColor': '#435334'
+    },
+    'Mater': {
+        'backgroundColor': '#eac696',
+        'primaryColor': '#765827',
+        'secondaryBackgroundColor': '#c8ae7d',
+        'textColor': '#65451f'
+    },
+    'Gastly': {
+        'backgroundColor': '#f3eeea',
+        'primaryColor': '#b0a695',
+        'secondaryBackgroundColor': '#ebe3d5',
+        'textColor': '#776b5d'
+    },
+    'Ocean': {
+        'backgroundColor': '#f6f4eb',
+        'primaryColor': '#749bc2',
+        'secondaryBackgroundColor': '#91c8e4',
+        'textColor': '#4682a9'
+    },
+    'Abyssal': {
+        'backgroundColor': '#93b1a6',
+        'primaryColor': '#183d3d',
+        'secondaryBackgroundColor': '#5c8374',
+        'textColor': '#040d12'
+    },
+    'Green': {
+        'backgroundColor': '#d0e7d2',
+        'primaryColor': '#79ac78',
+        'secondaryBackgroundColor': '#b0d9b1',
+        'textColor': '#618264'
+    },
+    'Sandy': {
+        'backgroundColor': '#fff2d8',
+        'primaryColor': '#bca37f',
+        'secondaryBackgroundColor': '#ead7bb',
+        'textColor': '#113946'
+    },
+    'Sunset': {
+        'backgroundColor': '#f2ecbe',
+        'primaryColor': '#c08261',
+        'secondaryBackgroundColor': '#e2c799',
+        'textColor': '#9a3b3b'
+    },
+    'Midnight': {
+        'backgroundColor': '#EEEEEE',
+        'primaryColor': '#393E46',
+        'secondaryBackgroundColor': '#00ADB5',
+        'textColor': '#222831'
+    }
+}
